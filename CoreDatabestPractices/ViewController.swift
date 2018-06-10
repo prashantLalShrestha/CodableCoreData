bd2a3556680d27e40c0700ca4f42b8e60a806db6
Prashant Shrestha <ram@prashants-macbook-air.local>
2018-06-10 20:41:35 +0545

    Initial Commit

---
+++ CoreDatabestPractices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDatabestPractices
+//
+//  Created by Prashant Shrestha on 6/10/18.
+//  Copyright © 2018 Prashant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
